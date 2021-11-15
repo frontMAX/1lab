b881f8c7c20b0b0302ea60b9d4752f4427db4b71
frontMAX <maxbjornkga@gmail.com>
2021-11-15 18:54:39 +0100

    the walk way is almost done.

--- main.js
+++ main.js
@@ -3,14 +3,17 @@ let start = document.getElementById ("start-content");
 let second = document.getElementById('second-content') ;
 let second2 = document.getElementById('second-content2');
 let third = document.getElementById('third-content');
+let lastWalk = document.getElementById('last-content');
 second.style.display = "none"  
 second2.style.display = "none"  
 third.style.display = "none"   
+lastWalk.style.display = "none"
  window.onload = main()
 
   function main (){
    addEventListener();
  }  
+ // the first side 
  function addEventListener(){  
 let startTheGame = alert('Välj en av dom två pulserande texterna som passar dig bäst '); }
  function skiLift() {
@@ -36,12 +39,52 @@ function sunOverThere(){
         second2.style.display = "none"
         third.style.display = "block"
     }
+    alert('Uppe på topppen, tryck på klar knappen för att njuta av dagens utsikt')
+}
+
+function lastBtn() {
+    if (third.style.display = "block"){
+        third.style.display = "none"
+        lastWalk.style.display = "block"
+    }
+   
+
 }
 
 
-function startOver() {
-    second.style.display = "none"  
-    second2.style.display = "none"  
-    third.style.display = "none" 
-    start.style.display = "block"
-}
+
+
+
+//     const answer = prompt ('ange ditt namn');
+//      username = answer;
+//      prepareStory ();
+// }
+// function prepareStory () {
+//     alert('asdada' '+ username +' 'asdadd');
+
+// //     const answer = prompt ('vad vill du göra? gi till vardagsrummer eller köket? skriv "vardagsrummet" eller "köket"')
+// //     if (answer === 'vardagsrummet'){
+// //         enterlivingroom();
+// //     } else if (answer === 'köket') {
+// //         enterkitchen ();
+// //     } else {
+// //         alert ('var god ange något av dom')
+// //         prepareStory();    
+// //     }   
+// // }
+
+
+
+
+
+
+
+
+
+
+// function startOver() {
+//     second.style.display = "none"  
+//     second2.style.display = "none"  
+//     third.style.display = "none" 
+//     start.style.display = "block"
+// }
