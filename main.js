de79a9f4b834ba1b70ed45415f00b8422476bcd2
frontMAX <maxbjornkga@gmail.com>
2021-11-15 13:34:35 +0100

    continue in the third section

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
+
 let start = document.getElementById ("start-content");  
-let second = document.getElementById('second-content') 
- second.style.display = "none"  
-  window.onload = main()  
-  
+let second = document.getElementById('second-content') ;
+let second2 = document.getElementById('second-content2');
+let third = document.getElementById('third-content');
+second.style.display = "none"  
+second2.style.display = "none"  
+third.style.display = "none"   
+ window.onload = main()
+
   function main (){
    addEventListener();
- }  function addEventListener(){  
-let startTheGame = alert('Välkommen till denna värld, där ditt mål är att ta dig till toppen och ner igen '); }
- function skiLift() { let start = document.getElementById ("start-content"); 
+ }  
+ function addEventListener(){  
+let startTheGame = alert('Välj en av dom två pulserande texterna som passar dig bäst '); }
+ function skiLift() {
+    //   let start = document.getElementById ("start-content"); 
     if(start.style.display = "block") {   
      start.style.display = "none"    
      second.style.display = "block" 
-  } }
+  } 
+  let jump = alert('Det går nu med en väldig fart ner. Du ser en stubbe vad gör du?')
+}
+
+function walk() {
+    if (start.style.display = "block"){
+        start.style.display = "none"
+        second2.style.display = "block"
+     }
+     alert("Du valde att gå du får lite olika känslor, vilket av alternativen väljer du?")
+    
+} 
+
+function sunOverThere(){
+    if (second2.style.display = "block"){
+        second2.style.display = "none"
+        third.style.display = "block"
+    }
+}
+
+
+function startOver() {
+    second.style.display = "none"  
+    second2.style.display = "none"  
+    third.style.display = "none" 
+    start.style.display = "block"
+}
