2d6ae87df838837c68af68926285cd3604eb1328
frontMAX <maxbjornkga@gmail.com>
2021-11-15 11:11:04 +0100

    First side of project

---
+++ main.js
@@ -0,0 +1,14 @@
+let start = document.getElementById ("start-content");  
+let second = document.getElementById('second-content') 
+ second.style.display = "none"  
+  window.onload = main()  
+  
+  function main (){
+   addEventListener();
+ }  function addEventListener(){  
+let startTheGame = alert('Välkommen till denna värld, där ditt mål är att ta dig till toppen och ner igen '); }
+ function skiLift() { let start = document.getElementById ("start-content"); 
+    if(start.style.display = "block") {   
+     start.style.display = "none"    
+     second.style.display = "block" 
+  } }
